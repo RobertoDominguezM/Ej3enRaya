87e25ab8bfcaeea7fb8ff7047454eb0b0db483ba
Roberto Domínguez <201902@CALASANZSALAMANCA.LOCAL>
2020-10-29 14:07:32 +0100

    fbd

---
+++ scripts/ejercicio2.js
@@ -0,0 +1,19 @@
+function Iniciar(){  
+    var i =
+    if(){
+
+    }else{
+
+    }
+}
+/*function dibujar(){
+    for(i=0; i<9; i++){
+    if(mapa[i] == 0) document.getElementById("c"+i).style="background-color: grey";
+    if(mapa[i] == 1) document.getElementById("c"+i).style="background-color: red";
+    if(mapa[i] == 2) document.getElementById("c"+i).style="background-color: blue"; 
+    }
+}*/
+function Desactivar(){
+    let iniciar = document.getElementsByClassName('iniciar');
+    iniciar.setAttribute('disabled', '');
+}
